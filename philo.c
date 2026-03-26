df74f07f0ee675795b5f8132b60d3057f4956132
LeonardStarekBusiness <136514236+LeonardStarekBusiness@users.noreply.github.com>
2026-03-26 15:25:05 +0100

    Add files via upload

--- philo.c
+++ philo.c
@@ -16,42 +16,48 @@ int	quit(int type)
 	return (-1);
 }
 
-long now_ms(t_philo *info)
+long	now_ms(t_philo *info)
 {
 	struct timeval	now;
 	long			diff;
 
 	gettimeofday(&now, NULL);
-	diff = (now.tv_sec - info->tv->tv_sec) * 1000 + (now.tv_usec - info->tv->tv_usec) / 1000;
+	diff = (now.tv_sec - info->tv->tv_sec) * 1000 + (now.tv_usec
+			- info->tv->tv_usec) / 1000;
 	return (diff);
 }
 
-void take_fork(t_philo *info)
+void	take_fork(t_philo *info)
 {
 	pthread_mutex_lock(info->mutex);
 	printf("%ld\t%d has taken a fork!\n", now_ms(info), info->n);
+	//usleep(5000);
 	pthread_mutex_unlock(info->mutex);
 }
 
-void think(t_philo *info)
+void	think(t_philo *info)
 {
 	pthread_mutex_lock(info->mutex);
 	printf("%ld\t%d is thinking!\n", now_ms(info), info->n);
+	//usleep(5000);
 	pthread_mutex_unlock(info->mutex);
 }
 
-void eat(t_philo *info)
+void	eat(t_philo *info)
 {
 	pthread_mutex_lock(info->mutex);
 	printf("%ld\t%d is eating!\n", now_ms(info), info->n);
+	//usleep(5000);
+	info->times_eaten++;
 	pthread_mutex_unlock(info->mutex);
 	usleep(info->time_to_eat * 1000);
 }
 
-void nap(t_philo *info)
+void	nap(t_philo *info)
 {
 	pthread_mutex_lock(info->mutex);
 	printf("%ld\t%d is sleeping!\n", now_ms(info), info->n);
+	//usleep(5000);
 	pthread_mutex_unlock(info->mutex);
 	usleep(info->time_to_sleep * 1000);
 }
@@ -61,15 +67,20 @@ void	start_simulation_leftist(t_philo *info)
 	while (1)
 	{
 		pthread_mutex_lock(&(info->fork));
-			take_fork(info);
-			pthread_mutex_lock(info->next_fork);
-				take_fork(info);
-				eat(info);
-			pthread_mutex_unlock(&(info->fork));
+		take_fork(info);
+		pthread_mutex_lock(info->next_fork);
+		take_fork(info);
+		eat(info);
+		pthread_mutex_unlock(&(info->fork));
 		pthread_mutex_unlock(info->next_fork);
 		nap(info);
 		think(info);
+		pthread_mutex_lock(info->mutex);
+		if (info->impending_doom)
+			break;
+		pthread_mutex_unlock(info->mutex);
 	}
+	pthread_mutex_unlock(info->mutex);
 }
 
 void	start_simulation_rightist(t_philo *info)
@@ -77,15 +88,20 @@ void	start_simulation_rightist(t_philo *info)
 	while (1)
 	{
 		pthread_mutex_lock(info->next_fork);
-			take_fork(info);
-				pthread_mutex_lock(&(info->fork));
-				take_fork(info);
-				eat(info);
-			pthread_mutex_unlock(info->next_fork);
+		take_fork(info);
+		pthread_mutex_lock(&(info->fork));
+		take_fork(info);
+		eat(info);
+		pthread_mutex_unlock(info->next_fork);
 		pthread_mutex_unlock(&(info->fork));
 		nap(info);
 		think(info);
+		pthread_mutex_lock(info->mutex);
+		if (info->impending_doom)
+			break ;
+		pthread_mutex_unlock(info->mutex);
 	}
+	pthread_mutex_unlock(info->mutex);
 }
 
 void	*thread_init(void *info_ptr)
@@ -93,22 +109,62 @@ void	*thread_init(void *info_ptr)
 	t_philo	*info;
 
 	info = (t_philo *)info_ptr;
-
 	pthread_mutex_lock(info->mutex);
-	//printf("%ld\t%d has spawned!\n", now_ms(info), info->n);
 	pthread_mutex_unlock(info->mutex);
-
-	if (info->n%2)
+	if (info->n % 2)
 		start_simulation_leftist(info);
 	else
 		start_simulation_rightist(info);
 	return (NULL);
 }
 
+void	*table_activities(void *info_ptr)
+{
+	t_philo	*info;
+	int		i;
+	int		have_eaten;
+	int		n_must_eat;
+	int		n_philos_exist;
+
+	info = (t_philo *)info_ptr;
+	have_eaten = 0;
+	pthread_mutex_lock(info->mutex);
+	n_must_eat = info[0].times_must_eat;
+	n_philos_exist = info[0].max_n;
+	pthread_mutex_unlock(info->mutex);
+	while (1)
+	{
+		i = 0;
+		while (i < n_philos_exist)
+		{
+			pthread_mutex_lock(info->mutex);
+			if (info[i].times_eaten >= n_must_eat)
+				have_eaten += 1;
+			pthread_mutex_unlock(info->mutex);
+			i++;
+		}
+		if (have_eaten == n_philos_exist)
+			break;
+		have_eaten = 0;
+		usleep(10000);
+	}
+	i = 0;
+	pthread_mutex_lock(info->mutex);
+	while (i < n_philos_exist)
+	{
+		info[i].impending_doom = 1;
+		i++;
+	}
+	pthread_mutex_unlock(info->mutex);
+	return (NULL);
+}
+
+
 int	main(int ac, char **av)
 {
 	int				i;
 	pthread_t		*threddy;
+	pthread_t		table;
 	t_philo			*infos;
 	pthread_mutex_t	mutex;
 	int				threads;
@@ -137,8 +193,10 @@ int	main(int ac, char **av)
 		infos[i].time_to_die = ft_atoi(av[2]);
 		infos[i].time_to_eat = ft_atoi(av[3]);
 		infos[i].time_to_sleep = ft_atoi(av[4]);
+		if (ac == 6)
+			infos[i].times_must_eat = ft_atoi(av[5]);
 		pthread_mutex_init(&(infos[i].fork), NULL);
-		infos[i].next_fork = &(infos[(i+1)%threads].fork);
+		infos[i].next_fork = &(infos[(i + 1) % threads].fork);
 		i++;
 	}
 	i = 0;
@@ -148,6 +206,7 @@ int	main(int ac, char **av)
 		i++;
 	}
 	sleep(1);
+	pthread_create(&table, NULL, &table_activities, infos);
 	pthread_mutex_unlock(&mutex);
 	i = 0;
 	while (i < threads)
--- philo.h
+++ philo.h
@@ -3,28 +3,41 @@
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <sys/time.h>
+# include <unistd.h>
 
 int					ft_atoi(const char *str);
+
+typedef struct s_table
+{
+	pthread_t		thread;
+	char			*haveEaten;
+	int				n_of_philos;
+}					t_table;
 
 typedef struct s_philo
 {
 	pthread_t		thread;
-	int				n;
-	int				max_n;
+	short				n;
+	short				max_n;
 
 	struct timeval	*tv;
 	useconds_t		time_to_die;
 	useconds_t		time_to_eat;
 	useconds_t		time_to_sleep;
-	
+
 	pthread_mutex_t	*mutex;
-	pthread_mutex_t fork;
-	pthread_mutex_t *next_fork;
+	pthread_mutex_t	fork;
+	pthread_mutex_t	*next_fork;
 
 	struct timeval	*last_supper;
-	char			*impending_doom;
+	short			times_must_eat;
+	short			times_eaten;
+	char			impending_doom;
+
+	t_table			*table;
 }					t_philo;
+
+
 
 #endif // PHILO_H
